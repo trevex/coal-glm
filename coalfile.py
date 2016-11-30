bd5f971e4deb9187aee2eb63252c518985ea5339
Nik Voss <niklas.voss@gmail.com@>
2016-11-30 10:49:42 +0100

    Fixed copying of selected version.

--- coalfile.py
+++ coalfile.py
@@ -11,10 +11,11 @@ class GlmFile(CoalFile):
         if "version" in self.settings:
             version = self.settings["version"]
             download(self.url % (version, version), self.zipfile)
-            unzip(self.zipfile, 'src')
+            unzip(self.zipfile, 'temp')
         else:
             git_clone(self.repo, 'master', 'src')
     def package(self):
         cp('src/glm', 'include/')
+        cp('temp/glm/glm', 'include/')
     def info(self, generator):
         generator.add_include_dir('include/')
